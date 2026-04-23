d225368ac076ce4e32baadd10a3bb3cf33cd598e
Nikita <nikitakatrich.kzn@gmail.com>
2026-04-23 12:25:07 +0300

    made logic with bottom menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,21 +1,46 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 import styled from 'styled-components';
 import countdownIcon from '../assets/icons/countdown.svg';
 import timerIcon from '../assets/icons/timer.svg';
 
 const Button = () => {
+    const [countdown, setCountdown] = useState(true)
+    const [timer, setTimer] = useState(false)
+
+  function callCountdown() {
+    setCountdown(true)
+    setTimer(false)
+  }
+
+  function callTimer() {
+    setCountdown(false)
+    setTimer(true)
+  }
+
+  const handleClick = (event) => {
+    const button = event.target.closest('button')
+    if (!button) return
+
+    const action = button.dataset.action
+    if (action === 'countdown') {
+      callCountdown()
+    } else if (action === 'timer') {
+      callTimer()
+    }
+  }
+
   return (
-    <StyledWrapper>
+    <StyledWrapper onClick={handleClick}>
       <div className="button-container">
-        <button className="button">
+        <button style={countdown ? styleCountdown : baseStyleBtnMenu} data-action="countdown">
           <img src={countdownIcon} alt="countdown" />
         </button>
-        <button className="button">
+        <button style={timer ? styleCountdown : baseStyleBtnMenu} data-action="timer">
           <img src={timerIcon} alt="timer" />
         </button>
       </div>
     </StyledWrapper>
-  );
+  )
 }
 
 const StyledWrapper = styled.div`
@@ -32,25 +57,47 @@ const StyledWrapper = styled.div`
     bottom: 15px;
     left: 50%;
     transform: translateX(-50%);
+    overflow: hidden;
   }
 
-  .button {
-    outline: 0 !important;
-    border: 0 !important;
-    width: 100%;
-    height: 100%;
-    border-radius: 50%;
-    background-color: transparent;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    color: #fff;
-    transition: all ease-in-out 0.1s;
-    cursor: pointer;
-  }
-
-  .button:hover {
+  .button-menu:hover {
     transform: scale(1.1);
   }`;
+
+  const baseStyleBtnMenu = {
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'transparent',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: '#fff',
+    transition: 'all ease-in-out 0.1s',
+    cursor: 'pointer'
+  }
+
+  const styleCountdown = {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "rgb(57, 0, 227)",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    color: "#fff",
+    transition: "all ease-in-out 0.1s",
+    cursor: "pointer"
+  }
+
+  const styleTimer = {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "rgb(57, 0, 227)",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    color: "#fff",
+    transition: "all ease-in-out 0.1s",
+    cursor: "pointer"
+  }
 
 export default Button;
